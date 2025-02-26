b36166c2c0cf1075dbac00432d67bf5162835221
LuzAlaves <luzzzz7@gmail.com>
2025-02-26 07:20:48 +0100

    Nombres python

---
+++ nombres.py
@@ -0,0 +1,13 @@
+# Demande à l'utilisateur d'entrer deux nombres
+num1 = float(input("Entrez le 1er nombre: "))
+num2 = float(input("Entrez le 2ème nombre: "))
+
+# Calcull des opérations demandées
+somme = num1 + num2
+difference = num1 - num2
+moyenne = (num1 + num2) / 2
+
+# Affichage des résultats
+print(f"Somme : {somme}")
+print(f"Différence : {difference}")
+print(f"Moyenne : {moyenne}")
