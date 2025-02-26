b06b30ebf3705f362a7625fbd2aab6d2aee8ce99
LuzAlaves <luzzzz7@gmail.com>
2025-02-26 07:59:00 +0100

    Modification par une fonction

--- nombres.py
+++ nombres.py
@@ -1,11 +1,17 @@
+def calculs(num1, num2):
+    # Calcull des opérations demandées
+    somme = num1 + num2
+    difference = num1 - num2
+    moyenne = (num1 + num2) / 2
+
+    return somme, difference, moyenne
+
 # Demande à l'utilisateur d'entrer deux nombres
 num1 = float(input("Entrez le 1er nombre: "))
 num2 = float(input("Entrez le 2ème nombre: "))
 
-# Calcull des opérations demandées
-somme = num1 + num2
-difference = num1 - num2
-moyenne = (num1 + num2) / 2
+# Appel de la fonction et récupération des résultats
+somme, difference, moyenne = calculs(num1, num2)
 
 # Affichage des résultats
 print(f"Somme : {somme}")
