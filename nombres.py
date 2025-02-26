b7168524a354ff288e920c39a81e6687ffa3d8fc
LuzAlaves <luzzzz7@gmail.com>
2025-02-26 10:15:14 +0100

    Ajout main

--- nombres.py
+++ nombres.py
@@ -6,14 +6,15 @@ def calculs(num1, num2):
 
     return somme, difference, moyenne
 
-# Demande à l'utilisateur d'entrer deux nombres
-num1 = float(input("Entrez le 1er nombre: "))
-num2 = float(input("Entrez le 2ème nombre: "))
+# Seulement exécuter si le script est lancé manuellement
+if __name__ == "__main__":
+    num1 = float(input("Entrez le 1er nombre: "))
+    num2 = float(input("Entrez le 2ème nombre: "))
 
-# Appel de la fonction et récupération des résultats
-somme, difference, moyenne = calculs(num1, num2)
+    # Appel de la fonction et récupération des résultats
+    somme, difference, moyenne = calculs(num1, num2)
 
-# Affichage des résultats
-print(f"Somme : {somme:.2f}")
-print(f"Différence : {difference:.2f}")
-print(f"Moyenne : {moyenne:.2f}")
+    # Affichage des résultats
+    print(f"Somme : {somme:.2f}")
+    print(f"Différence : {difference:.2f}")
+    print(f"Moyenne : {moyenne:.2f}")
